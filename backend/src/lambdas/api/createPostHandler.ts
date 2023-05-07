19a4fdd1278bb71e8f7fdca3f1653ef612201720
EnrisKumi <ekumi20@epoka.edu.al>
2023-05-07 19:55:34 +0200

    crate post handler

---
+++ backend/src/lambdas/api/createPostHandler.ts
@@ -0,0 +1,15 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { parseGwEvent } from "../../../lib/utils/parseGwEvent";
+import { gwResponse } from "../../../lib/utils/gwResponse";
+import { gwError } from "../../../lib/utils/gwError";
+
+
+export const handler:APIGatewayProxyHandler =async (event) => {
+    const {body, identity} = parseGwEvent(event)
+
+    try {
+        return gwResponse(body)
+    } catch (error) {
+        return gwError(error)
+    }
+}
