a914652d2bc2f1e44d2f8dcba521e8e925554500
EnrisKumi <ekumi20@epoka.edu.al>
2023-05-21 17:22:10 +0200

    fixed post confirm handler

--- backend/src/lambdas/cognitoTriggers/postConfirmationHandler.ts
+++ backend/src/lambdas/cognitoTriggers/postConfirmationHandler.ts
@@ -1,9 +1,11 @@
+import { PostConfirmationTriggerHandler } from "aws-lambda";
 import { connectDataBase } from "../../data/db/connection";
 import { User } from "../../data/models/User";
 
 
-export const handler = async (event: any, context: any) => {
-    console.log(event)
+export const handler: PostConfirmationTriggerHandler = async (event, context, callback) => {
+    console.log(JSON.stringify(event))
+    console.log(context)
     context.callbackWaitsForEmptyEventLoop = false;
     const cognitoId = event.request.userAttributes.sub;
     const username = event.userName
@@ -13,41 +15,30 @@ export const handler = async (event: any, context: any) => {
     const date = Date.now()
     console.log(cognitoId, username, location, bio, prfilePicture, date)
 
-    try {
-        await connectDataBase()
+    if(event.triggerSource === 'PostConfirmation_ConfirmSignUp'){
+        try {
+            await connectDataBase()
+    
+            const user = await User.findOne({ userCognitoId: cognitoId });
+                User.init();
+                const newUser = new User({
+                    userCognitoId: cognitoId,
+                    username: username,
+                    location: location,
+                    bio: bio,
+                    prfilePicture: prfilePicture,
+                    date: date
+                });
+                await newUser.save()
 
-        const user = await User.findOne({ userCognitoId: cognitoId });
-
-        if (user) {
-            console.log("!!!!!!!!!!!!!!!!!!!!! EXISTS !!!!!!!!!!!!!!!!!!!!!!!!!!")
-        } else {
-            User.init();
-            const newUser = new User({
-                userCognitoId: cognitoId,
-                username: username,
-                location: location,
-                bio: bio,
-                prfilePicture: prfilePicture,
-                date: date
-            });
-            await newUser.save()
-
-
-            return {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    "Allow": "GET, OPTIONS, POST",
-                    "Access-Control-Allow-Methods": "GET, OPTIONS, POST",
-                    "Access-Control-Allow-Headers": "*"
-                },
-                statusCode: 200,
-                body: JSON.stringify(newUser),
-            };
+            callback(null, event)
         }
+        catch (error) {
+            console.log(error)
+            callback(new Error('Something went wrong!'))
+        }
+    } else {
+        callback(null, event)
     }
-    catch (error) {
-        console.log(error)
-        return (error);
-    }
+    callback(null, event)
 }
